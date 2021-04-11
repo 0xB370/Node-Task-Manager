efa2dda54bbc2b1838d910de3a1a8fc741ada662
Alberto Mansilla <mansilla_alberto@outlook.com>
2021-04-11 01:39:44 -0300

    Rendering Validation Errors

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,36 +1,40 @@
 <template>
     <h1 class="my-5">Registro de Usuario</h1>
-    <form @submit.prevent="registrarUsuario({email, password: pass1}); limpiarForm()">
+    <div class="alert alert-danger" v-if="error.tipo !== null">
+        {{ error.mensaje }}
+    </div>
+    <form @submit.prevent="procesarForm()">
         <input 
-        type="email" 
-        placeholder="E-mail"
-        class="form-control my-2"
-        v-model.trim="email"
-    >
-    <input 
-        type="password" 
-        placeholder="Password"
-        class="form-control my-2"
-        v-model.trim="pass1"
-    >
-    <input 
-        type="password"
-        placeholder="Repetir Password"
-        class="form-control my-2"
-        v-model.trim="pass2"
-    >
-    <button 
-        type="submit" 
-        class="btn btn-primary"
-        :disabled="bloquear"
-    >
-        Registrar
-    </button>
+            type="email" 
+            placeholder="E-mail"
+            class="form-control my-2"
+            v-model.trim="email"
+            :class="[error.tipo === 'email' ? 'is-invalid' : '']"
+        >
+        <input 
+            type="password" 
+            placeholder="Password"
+            class="form-control my-2"
+            v-model.trim="pass1"
+        >
+        <input 
+            type="password"
+            placeholder="Repetir Password"
+            class="form-control my-2"
+            v-model.trim="pass2"
+        >
+        <button 
+            type="submit" 
+            class="btn btn-primary"
+            :disabled="bloquear"
+        >
+            Registrar
+        </button>
     </form>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 export default {
     data() {
@@ -41,6 +45,7 @@ export default {
         }
     },
     computed: {
+        ...mapState(['error']),
         bloquear() {
             return !( (this.email.includes("@")) && 
                 (this.pass1.length > 5) && (this.pass1 === this.pass2) )
@@ -48,7 +53,11 @@ export default {
     },
     methods: {
         ...mapActions(['registrarUsuario']),
-        limpiarForm() {
+        async procesarForm() {
+            await this.registrarUsuario({email: this.email, password: this.pass1})
+            if (this.error.tipo === null) {
+                return
+            }
             this.email = ''
             this.pass1 = ''
             this.pass2 = ''
